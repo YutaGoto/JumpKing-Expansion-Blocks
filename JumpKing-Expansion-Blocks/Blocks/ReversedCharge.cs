d7e031c656ec87bac98a6c86ef6f35f3432c898a
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-08-16 21:25:44 +0900

    :bug: fix block type

--- JumpKing-Expansion-Blocks/Blocks/ReversedCharge.cs
+++ JumpKing-Expansion-Blocks/Blocks/ReversedCharge.cs
@@ -3,10 +3,35 @@ using Microsoft.Xna.Framework;
 
 namespace JumpKing_Expansion_Blocks.Blocks
 {
-    public class ReversedCharge: BoxBlock, IBlockDebugColor
+    public class ReversedCharge: IBlock, IBlockDebugColor
     {
-        protected override bool canBlockPlayer => false;
-        public ReversedCharge(Rectangle p_collidar) :  base(p_collidar) { }
+        private readonly Rectangle m_collider;
+
+        public ReversedCharge(Rectangle p_collidar)
+        {
+            m_collider = p_collidar;
+        }
+
+        public Rectangle GetRect()
+        {
+            return m_collider;
+        }
+
+        public BlockCollisionType Intersects(Rectangle p_hitbox, out Rectangle p_intersection)
+        {
+            bool ret = m_collider.Intersects(p_hitbox);
+
+            if (ret)
+            {
+                p_intersection = Rectangle.Intersect(p_hitbox, m_collider);
+                return BlockCollisionType.Collision_NonBlocking;
+            }
+            else
+            {
+                p_intersection = new Rectangle(0, 0, 0, 0);
+                return BlockCollisionType.NoCollision;
+            }
+        }
 
         public Color DebugColor { get { return Constants.ColorCodes.CODE_REVERSED_CHARGE; } }
     }
