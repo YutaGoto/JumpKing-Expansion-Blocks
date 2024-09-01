a071ef5df49ab95afd8b50467ae8932930131d85
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-09-01 14:53:20 +0900

    :recycle: remove unnecessary import

--- JumpKing-Expansion-Blocks/Behaviours/DoubleJump.cs
+++ JumpKing-Expansion-Blocks/Behaviours/DoubleJump.cs
@@ -4,10 +4,6 @@ using JumpKing.BodyCompBehaviours;
 using JumpKing.Level;
 using JumpKing.Player;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JumpKing_Expansion_Blocks.Behaviours
 {
