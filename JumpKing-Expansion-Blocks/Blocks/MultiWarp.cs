5642fdfbff36fcadd78dc2b3f57664456d31027d
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-12-01 12:34:13 +0900

    :sparkles: add MultiWarp block

---
+++ JumpKing-Expansion-Blocks/Blocks/MultiWarp.cs
@@ -0,0 +1,26 @@
+﻿using JumpKing.Level;
+using Microsoft.Xna.Framework;
+
+namespace JumpKing_Expansion_Blocks.Blocks
+{
+    internal class MultiWarp: BoxBlock, IBlockDebugColor
+    {
+        protected override bool canBlockPlayer => false;
+
+        private readonly Rectangle m_collider;
+        public int ToScreenNo { get; private set; }
+
+        public MultiWarp(Rectangle collider, byte toScreenNo = 1): base (collider)
+        { 
+            m_collider = collider;
+            ToScreenNo = (int)toScreenNo;
+        }
+
+        public Color DebugColor { get { return new Color(ToScreenNo, Constants.MultiWarpColorCodes.MULTI_WARP_G, Constants.MultiWarpColorCodes.MULTI_WARP_B); } }
+
+        public Rectangle GetRect()
+        {
+            return m_collider;
+        }
+    }
+}
