6b2da46a62bdd8a9b4f7660aae04e79d97790688
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-12-22 00:18:01 +0900

    :sparkles: add QuickMove Block

---
+++ JumpKing-Expansion-Blocks/Behaviours/QuickMove.cs
@@ -0,0 +1,65 @@
+﻿using JumpKing.API;
+using JumpKing.BodyCompBehaviours;
+using JumpKing.Level;
+using JumpKing.Player;
+using System.Linq;
+
+namespace JumpKing_Expansion_Blocks.Behaviours
+{
+    public class QuickMove: IBlockBehaviour
+    {
+        public float BlockPriority => 3f;
+
+        public bool IsPlayerOnBlock { get; set; }
+        private float ratio = 1.0f;
+
+        public bool AdditionalXCollisionCheck(AdvCollisionInfo info, BehaviourContext behaviourContext)
+        {
+            return false;
+        }
+
+        public bool AdditionalYCollisionCheck(AdvCollisionInfo info, BehaviourContext behaviourContext)
+        {
+            return false;
+        }
+
+        public bool ExecuteBlockBehaviour(BehaviourContext behaviourContext)
+        {
+
+            if (behaviourContext?.CollisionInfo?.PreResolutionCollisionInfo != null)
+            {
+                IsPlayerOnBlock = behaviourContext.CollisionInfo.PreResolutionCollisionInfo.IsCollidingWith<Blocks.QuickMove>();
+            }
+
+            return true;
+        }
+
+        public float ModifyXVelocity(float inputXVelocity, BehaviourContext behaviourContext)
+        {
+            BodyComp bodyComp = behaviourContext.BodyComp;
+            
+
+            if (IsPlayerOnBlock)
+            {
+                if (behaviourContext.LastFrameCollisionInfo?.PreResolutionCollisionInfo != null)
+                {
+                    Blocks.QuickMove quickMove = behaviourContext.LastFrameCollisionInfo.PreResolutionCollisionInfo.GetCollidedBlocks<Blocks.QuickMove>().FirstOrDefault() as Blocks.QuickMove;
+                    ratio = 1.0f + quickMove.Ratio * 0.1f;
+                }
+            }
+
+            return inputXVelocity * ratio;
+        }
+
+        public float ModifyYVelocity(float inputYVelocity, BehaviourContext behaviourContext)
+        {
+            return inputYVelocity;
+        }
+
+        public float ModifyGravity(float inputGravity, BehaviourContext behaviourContext)
+        {
+            return inputGravity;
+        }
+
+    }
+}
---
+++ JumpKing-Expansion-Blocks/Constants/QuickMoveCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace JumpKing_Expansion_Blocks.Constants
+{
+    public class QuickMoveCodes
+    {
+        public static readonly int QUICK_MOVE_R = 195;
+        public static readonly int QUICK_MOVE_G_MIN = 0;
+        public static readonly int QUICK_MOVE_G_MAX = 255;
+        public static readonly int QUICK_MOVE_B = 255;
+    }
+}
