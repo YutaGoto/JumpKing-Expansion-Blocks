6d040eb55cbcc16c670a592942d39335f12a98f8
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-03-05 21:07:56 +0900

    :sparkles: add SideLock Block

---
+++ JumpKing-Expansion-Blocks/Constants/SideLockColorCodes.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace JumpKing_Expansion_Blocks.Constants
+{
+    class SideLockColorCodes
+    {
+        public static readonly Color CODE_ONE_WAY_STOP_KING = new Color(99, 0, 0);
+
+        public static readonly int SIDE_LOCK_R_RIGHT = 98;
+        public static readonly int SIDE_LOCK_R_LEFT = 99;
+        public static readonly int SIDE_LOCK_G = 6;
+        public static readonly int SIDE_LOCK_B = 6;
+    }
+}
