3986065969c4b3b1436f0abe93fa41a8447b146a
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-03-12 00:00:37 +0900

    :sparkles: add SoftPlatform

---
+++ JumpKing-Expansion-Blocks/Blocks/SoftPlatform.cs
@@ -0,0 +1,13 @@
+﻿using JumpKing.Level;
+using Microsoft.Xna.Framework;
+
+namespace JumpKing_Expansion_Blocks.Blocks
+{
+    public class SoftPlatform: BoxBlock, IBlockDebugColor
+    {
+        protected override bool canBlockPlayer => false;
+        public SoftPlatform(Rectangle p_collider) : base(p_collider) { }
+
+        public Color DebugColor { get { return Constants.ColorCodes.CODE_SOFT_PLATFORM; } }
+    }
+}
