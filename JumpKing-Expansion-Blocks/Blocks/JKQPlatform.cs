9cc39beab6b71a63a02d824280db53328351d8b4
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-03-12 00:03:44 +0900

    :sparkles: add JQK Platform Block

---
+++ JumpKing-Expansion-Blocks/Blocks/JKQPlatform.cs
@@ -0,0 +1,13 @@
+﻿using JumpKing.Level;
+using Microsoft.Xna.Framework;
+
+namespace JumpKing_Expansion_Blocks.Blocks
+{
+    public class JkqPlatform: BoxBlock, IBlockDebugColor
+    {
+        protected override bool canBlockPlayer => false;
+        public JkqPlatform(Rectangle p_collider) : base(p_collider) { }
+
+        public Color DebugColor { get { return Constants.ColorCodes.CODE_JKQ_PLATFORM; } }
+    }
+}
