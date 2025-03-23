e266a4ff0704c11c5e8ea4ad2d814a7612bf33fa
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-03-23 16:11:11 +0900

    :sparkles: add cutom power Reflector

--- JumpKing-Expansion-Blocks/Blocks/Reflector.cs
+++ JumpKing-Expansion-Blocks/Blocks/Reflector.cs
@@ -5,8 +5,21 @@ namespace JumpKing_Expansion_Blocks.Blocks
 {
     public class Reflector: BoxBlock, IBlockDebugColor
     {
-        public Reflector(Rectangle p_collider) : base(p_collider) { }  
+        public readonly Rectangle collider;
+        public int Ratio { get; private set; }
 
-        public Color DebugColor { get { return Constants.ColorCodes.CODE_REFLECTOR; } }
+        public Reflector(Rectangle p_collider, byte rario = 1) : base(p_collider)
+        {
+            collider = p_collider;
+            Ratio = (int)rario;
+        }
+
+        public Color DebugColor
+        {
+            get
+            {
+                return new Color(Ratio, Constants.ReflectorWallCodes.REFLECTOR_G, Constants.ReflectorWallCodes.REFLECTOR_B);
+            }
+        }
     }
 }
