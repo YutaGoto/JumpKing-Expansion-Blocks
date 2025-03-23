3dcaea8c67a41582c469a80d3b031a7c16370895
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-03-23 21:39:42 +0900

    :sparkles: update upper

--- JumpKing-Expansion-Blocks/Constants/ReflectorWallCodes.cs
+++ JumpKing-Expansion-Blocks/Constants/ReflectorWallCodes.cs
@@ -2,8 +2,9 @@
 {
     class ReflectorWallCodes
     {
+        // 1.0 ~ 8.0
         public static readonly int REFLECTOR_R_MIN = 182;
-        public static readonly int REFLECTOR_R_MAX = 242;
+        public static readonly int REFLECTOR_R_MAX = 252;
         public static readonly int REFLECTOR_G = 0;
         public static readonly int REFLECTOR_B = 192;
     }
