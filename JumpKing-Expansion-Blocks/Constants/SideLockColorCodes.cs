19dc5d0604d0a8c51a0fb1caa61c5bf8e720566f
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-04-07 00:23:51 +0900

    :fire: remove unnecessary comment, color

--- JumpKing-Expansion-Blocks/Constants/SideLockColorCodes.cs
+++ JumpKing-Expansion-Blocks/Constants/SideLockColorCodes.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace JumpKing_Expansion_Blocks.Constants
+﻿namespace JumpKing_Expansion_Blocks.Constants
 {
     class SideLockColorCodes
     {
-        public static readonly Color CODE_ONE_WAY_STOP_KING = new Color(99, 0, 0);
-
         public static readonly int SIDE_LOCK_R_RIGHT = 98;
         public static readonly int SIDE_LOCK_R_LEFT = 99;
         public static readonly int SIDE_LOCK_G = 6;
--- JumpKing-Expansion-Blocks/Constants/TrapHoppingCodes.cs
+++ JumpKing-Expansion-Blocks/Constants/TrapHoppingCodes.cs
@@ -2,8 +2,6 @@
 {
     class TrapHoppingCodes
     {
-        // public static readonly Color CODE_TRAP_HOPPING = new Color(193, 1, 193);
-
         public static readonly int TRAP_HOPPING_R = 193;
 
         public static readonly int TRAP_HOPPING_G_RIGHT = 1;
