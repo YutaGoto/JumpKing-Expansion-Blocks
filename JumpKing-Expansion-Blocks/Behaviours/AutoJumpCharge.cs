1e1fcff2b526a2b3cf0dcf64532290b16bebe206
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-04-27 19:37:15 +0900

    :sparkles: add AutoJumpCharge Block

---
+++ JumpKing-Expansion-Blocks/Behaviours/AutoJumpCharge.cs
@@ -0,0 +1,30 @@
+﻿using JumpKing.API;
+using JumpKing.BodyCompBehaviours;
+using JumpKing.Level;
+
+namespace JumpKing_Expansion_Blocks.Behaviours
+{
+    internal class AutoJumpCharge: IBlockBehaviour
+    {
+        public float BlockPriority => 2f;
+
+        public bool IsPlayerOnBlock { get; set; }
+
+        public AutoJumpCharge() { }
+
+        public bool ExecuteBlockBehaviour(BehaviourContext behaviourContext)
+        {
+            if (behaviourContext.CollisionInfo?.PreResolutionCollisionInfo != null)
+            {
+                IsPlayerOnBlock = behaviourContext.CollisionInfo.PreResolutionCollisionInfo.IsCollidingWith<Blocks.AutoJumpCharge>();
+            }
+            return true;
+        }
+
+        public bool AdditionalXCollisionCheck(AdvCollisionInfo info, BehaviourContext behaviourContext) => false;
+        public bool AdditionalYCollisionCheck(AdvCollisionInfo info, BehaviourContext behaviourContext) => false;
+        public float ModifyGravity(float inputGravity, BehaviourContext behaviourContext) => inputGravity;
+        public float ModifyXVelocity(float inputXVelocity, BehaviourContext behaviourContext) => inputXVelocity;
+        public float ModifyYVelocity(float inputYVelocity, BehaviourContext behaviourContext) => inputYVelocity;
+    }
+}
---
+++ JumpKing-Expansion-Blocks/Blocks/AutoJumpCharge.cs
@@ -0,0 +1,13 @@
+﻿using JumpKing.Level;
+using Microsoft.Xna.Framework;
+
+namespace JumpKing_Expansion_Blocks.Blocks
+{
+    public class AutoJumpCharge: BoxBlock, IBlockDebugColor
+    {
+        protected override bool canBlockPlayer => false;
+        public AutoJumpCharge(Rectangle p_collider) : base(p_collider) { }
+
+        public Color DebugColor { get { return Constants.ColorCodes.CODE_AUTO_JUMP_CHARGE; } }
+    }
+}
